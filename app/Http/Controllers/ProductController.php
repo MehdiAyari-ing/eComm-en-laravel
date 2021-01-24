9edd913640598b38379f10ef00474daa417df277
Mehdi AYARI <mehdi.ayari.ing@gmail.com>
2021-01-24 18:03:33 +0100

    login down

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    function index()
+{
+    
+    return "welcom to product page";
+}
+
+}
